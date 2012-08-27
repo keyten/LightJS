bc2b0affbca7222012c97a301b04c1ed7de3ac78
Dmitriy <mirosh96@mail.ru>
2012-08-27 16:26:34 +0400

    Added first code

--- light.js
+++ light.js
@@ -5,4 +5,31 @@
  * Released under the MIT License
  * 
  */
- 
+ Light = lr = $ = (function(window, undefined){
+
+ 	var lr = function(selector, context){}
+
+ 	lr.version = 0.1;
+
+
+ 	var objProto = Object.prototype;
+
+ 	lr.each = function(obj, fn){
+
+ 		if(+obj.length == obj.length){
+ 			for(var i = 0, l = obj.length; i < l; i++)
+ 				if(objProto.hasOwnProperty.call(obj, i))
+ 					fn.call(obj, obj[i], i);
+ 		}
+ 		else {
+ 			for(var i in obj)
+ 				if(objProto.hasOwnProperty.call(obj, i))
+ 					fn.call(obj, obj[i], i);
+ 		}
+
+ 	}
+
+
+ 	return lr;
+
+ })(window);
